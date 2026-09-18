5ca68350d4cbb2cd4cf5399c85cce42c6510f80d
sum9548 <sum954866@gmail.com>
2026-09-18 13:40:59 +0530

    Refactor cookieOptions for environment handling
    Updated cookieOptions to use environment variable for sameSite.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,12 +2,6 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import genToken from "../config/token.js";
 
-// const cookieOptions = {
-//   httpOnly: true,
-//   maxAge: 7 * 24 * 60 * 60 * 1000,
-//   sameSite: "production" ? "none" : "lax",
-//   secure: process.env.NODE_ENVIRONMENT === "production",
-// };
 const cookieOptions = {
   httpOnly: true,
   maxAge: 7 * 24 * 60 * 60 * 1000,
